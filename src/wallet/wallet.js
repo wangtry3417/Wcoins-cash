427d52f21528a260db0ac820ec32a381cc9b4eb9
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-08-23 15:13:23 +0800

    Update wallet.js

--- src/wallet/wallet.js
+++ src/wallet/wallet.js
@@ -1,2 +1,17 @@
-const { sha256 } = require("../crypto/crypto");
+const elliptic = require('elliptic');
+const ec = new elliptic.ec('secp256k1');
 
+// 生成私鑰
+const generatePrivateKey = () => {
+  return ec.genKeyPair().getPrivate('hex');
+};
+
+// 從私鑰中獲取公鑰
+const getPublicKey = (privateKey) => {
+  return ec.keyFromPrivate(privateKey).getPublic('hex');
+};
+
+module.exports = {
+  generatePrivateKey,
+  getPublicKey
+};
