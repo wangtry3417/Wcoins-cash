43190baa24a257b41cfc35db95e591ca70b9b1c8
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-08-23 15:44:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const crypto = require("./crypto/crypto");
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('balance.sqlite');
 
+// 建立餘額表
+db.run(`
+  CREATE TABLE IF NOT EXISTS wallets (
+    public_key TEXT PRIMARY KEY,
+    balance DECIMAL(18,8) NOT NULL
+  )
+`);
 
 module.exports = {
   crypto
