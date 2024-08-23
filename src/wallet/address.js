96220851072c7082f9790902c969c262a230fa9d
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-08-23 14:56:31 +0800

    Update address.js

--- src/wallet/address.js
+++ src/wallet/address.js
@@ -1,1 +1,7 @@
+const { sha256 } = require("../crypto/crypto");
+const crypto = require("crypto");
 
+const generateAddress = (user) => {
+  address = `${user}/${crypto.randomInt(199)}`;
+  return sha256(address);
+}
