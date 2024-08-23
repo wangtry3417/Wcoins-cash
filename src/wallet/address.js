6d3316fb63697a660beb76369ea79e65e89fcf16
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-08-23 15:19:35 +0800

    Update address.js

--- src/wallet/address.js
+++ src/wallet/address.js
@@ -1,6 +1,6 @@
 const { sha256 } = require("../crypto/crypto");
 const crypto = require("crypto");
-const { generatePrivateKey, getPublicKey } = require('./wallet');
+const { generatePrivateKey, getPublicKey, setInitialBalance } = require('./wallet');
 
 const generateAddress = (user, pw) => {
   // 生成私鑰
@@ -9,8 +9,11 @@ const generateAddress = (user, pw) => {
   // 獲取公鑰
   const publicKey = getPublicKey(privateKey);
 
+  // 設置初始餘額
+  const balance = setInitialBalance(privateKey);
+
   // 生成地址
-  const address = `${user}/${crypto.randomInt(199)}/${pw}/${publicKey}`;
+  const address = `${user}/${crypto.randomInt(199)}/${pw}/${publicKey}/${balance}`;
   return sha256(address);
 };
 
