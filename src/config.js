77481ea2ac048aea6243bf68740a48bcb11bf5bc
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-09-01 14:10:31 +0800

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const { Client } = require("pg");
+
+const client = new Client({
+  user: "default",
+  host: "ep-hidden-salad-a1a7pob9-pooler.ap-southeast-1.aws.neon.tech",
+  database: "verceldb",
+  password: "Gd2MsST3QYWF",
+  port: 5432
+});
