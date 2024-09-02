d99c45bc3d7d39203c8b6bf7f8f77696752a2f31
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-09-02 11:52:25 +0800

    Update block.js

--- src/blockchain/block.js
+++ src/blockchain/block.js
@@ -1,1 +1,18 @@
+const { sha256 } = require("../crypto/sha256");
 
+class Block {
+  constructor(block = 1, prevHash = null, hash) {
+    this.block = block;
+    this.prevHash = prevHash;
+    this.timestamp = Date.now();
+    this.hash = hash;
+
+    if (this.prevHash === null) {
+      this.block = 1;
+    }
+  }
+}
+
+module.exports = {
+  Block
+}
