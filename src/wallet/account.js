cacae9bc467478ce5ac33e83ece1e7c93750ecf9
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-09-03 14:15:20 +0800

    Update account.js

--- src/wallet/account.js
+++ src/wallet/account.js
@@ -1,3 +1,7 @@
 const { generatePrivateKey,getPublicKey } = require("./wallet");
 const { client } = require("../config");
 
+const createAcc = () => {
+  var prk = generatePrivateKey();
+  var puk = getPublicKey(prk);
+};
