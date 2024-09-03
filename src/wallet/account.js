a1db48ade4be1329d75a0f5eafcf408fbf080a58
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-09-03 14:22:30 +0800

    Update account.js

--- src/wallet/account.js
+++ src/wallet/account.js
@@ -6,15 +6,19 @@ const createAcc = () => {
   var puk = getPublicKey(prk);
   try {
 
-    // 創建表的 SQL 命令
+    // 創建帳戶的 SQL 命令
     await client.query(`
-      
+      INSERT INTO public.wallets (public_key, private_key, balance) VALUES ('${puk}', '${prk}','0');
     `);
 
-    console.log('已經建立了資料表');
+    console.log('已經建立了新帳戶');
   } catch (err) {
-    console.error('建立資料表錯誤：', err.stack);
+    console.error('建立帳戶錯誤：', err.stack);
   } finally {
     await client.end();
   }
+};
+
+module.exports = {
+  createAcc
 };
