d2ab11bdefd5fbbc780fd064c6820e03bdb77de5
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-09-03 14:17:47 +0800

    Update account.js

--- src/wallet/account.js
+++ src/wallet/account.js
@@ -4,4 +4,17 @@ const { client } = require("../config");
 const createAcc = () => {
   var prk = generatePrivateKey();
   var puk = getPublicKey(prk);
+  try {
+
+    // 創建表的 SQL 命令
+    await client.query(`
+      
+    `);
+
+    console.log('已經建立了資料表');
+  } catch (err) {
+    console.error('建立資料表錯誤：', err.stack);
+  } finally {
+    await client.end();
+  }
 };
