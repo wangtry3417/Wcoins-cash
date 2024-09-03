721c50f0dfc98ffbe3c677a01f58cf344d71f885
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-09-03 14:23:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 const { sha256 } = require("./crypto/sha256");
 const wallet = require("./wallet/wallet");
+const { createAcc } = require("./wallet/account");
 const { client } = require("./config");
 
 const run = async () => {
   try {
     await client.connect();
     console.log('成功連接至 PostgreSQL!');
-
-    // 創建表的 SQL 命令
-    await client.query(`
-      CREATE TABLE IF NOT EXISTS wallets (
-        id TEXT PRIMARY KEY,
-        public_key TEXT NOT NULL,
-        private_key TEXT NOT NULL,
-        balance DECIMAL(18,8) NOT NULL
-      );
-    `);
-
-    console.log('已經建立了資料表');
+    createAcc();
   } catch (err) {
     console.error('建立資料表錯誤：', err.stack);
   } finally {
